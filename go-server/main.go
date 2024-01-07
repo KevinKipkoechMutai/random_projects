31f01375edd3bbc69581eae286baa3867b99de5f
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2024-01-07 10:01:27 +0300

    2-route go server

--- go-server/main.go
+++ go-server/main.go
@@ -6,20 +6,28 @@ import (
 	"net/http"
 )
 
-func helloHandler(w, r *)  {
-	if r.URL.path != "/hello" {
+func helloHandler(w http.ResponseWriter, r *http.Request)  {
+	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
-	if r.method != "GET" {
+	if r.Method != "GET" {
 		http.Error(w, "method is not supported", http.StatusNotFound)
 	}
 
-	fmt.fprintf(w, "hello!")
+	fmt.Fprintf(w, "Hello!")
 }
 
-func formHandler()  {
+func formHandler(w http.ResponseWriter, r *http.Request)  {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+	}
+	fmt.Fprintf(w, "POST Request successful")
 	
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s \n", name)
+	fmt.Fprintf(w, "Address = %s \n", address)
 }
 
 func main()  {
@@ -29,7 +37,7 @@ func main()  {
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServer(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
