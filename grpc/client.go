f7994a3062899a7a90a5084cac59977bab9c893d
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2024-01-09 12:23:15 +0300

    grpc-based chat server

---
+++ grpc/client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"chat-grpc/chat"
+	"context"
+	"log"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var conn *grpc.ClientConn
+
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("could not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := chat.NewChatServiceClient(conn)
+
+	message := chat.Message{
+		Body: "Hello from the client!",
+	}
+
+	response, err := c.SayHello(context.Background(), &message)
+
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+
+	log.Printf("Response from server: %s", response.Body)
+}
---
+++ grpc/go.mod
@@ -0,0 +1,5 @@
+module chat-grpc
+
+go 1.21.5
+
+require github.com/golang/protobuf v1.5.3 // indirect
