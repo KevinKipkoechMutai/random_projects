28c5e33c330fbad178bb69f672932c870d724be2
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2024-01-10 13:58:45 +0300

    routes creation

---
+++ book-management-store/pkg/routes/bookstore-routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/KevinKipkoechMutai/random_projects/book-management-store/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func (router *mux.Router)  {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{book_id}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{book_id}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{book_id}", controllers.DeleteBook).Methods("DELETE")
+}
